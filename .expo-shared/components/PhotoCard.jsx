b272fd79b45ab12eaa6973377a3233f67bd478cc
dolsen0 <derek.olsen@gmail.com>
2022-08-17 15:22:14 -0400

    added Avatar

--- .expo-shared/components/PhotoCard.jsx
+++ .expo-shared/components/PhotoCard.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-import { View, Image, Text} from 'react-native'
+import { View, Image, Text } from "react-native";
 import { photoCardStyles } from "../../assets/styles";
+import { Avatar } from "react-native-elements/dist/avatar/Avatar";
 
-function PhotoCard({photo}){
-    const BASE_URL = "https://source.unsplash.com/random?cat="
+function PhotoCard({ photo }) {
+  const BASE_URL = "https://source.unsplash.com/random?puppy=";
 
-    return(
-        <>
-        <View style={photoCardStyles.cardContainer}>
-            <View style = {photoCardStyles.cardHeader}>
+  return (
+    <>
+      <View style={photoCardStyles.cardContainer}>
+        <View style={photoCardStyles.cardHeader}>
+            <Avatar rounded size = "small" source={{uri: "https://randomuser.me/api/portraits/men/1.jpg"}}/>
 
-                <View style = {photoCardStyles.cardHeaderText}>
+          <View style={photoCardStyles.cardHeaderText}>
             <Text>Header Text</Text>
-            <Text style = {photoCardStyles}>Location</Text>
-
-                </View>
-            </View>
-            <Image />
+            <Text style={photoCardStyles}>Location</Text>
+          </View>
         </View>
+        <Image />
+      </View>
 
-        <Image source={{uri: BASE_URL + 1}} style={photoCardStyles.cardImage}/>
-        <Text style = {photoCardStyles.cardLocation}>Text</Text>
-        </>
-    )
-    
+      <Image source={{ uri: BASE_URL + 1 }} style={photoCardStyles.cardImage} />
+      <Text style={photoCardStyles.cardLocation}>Text</Text>
+    </>
+  );
 }
 
-
-export default PhotoCard
-
+export default PhotoCard;
